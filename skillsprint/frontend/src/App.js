936c78a1b3cb97529711267a816a9f25aa9e724e
Jordan Welborn <jordanwelborn2002@gmail.com>
2025-04-09 23:09:51 -0400

    made intial changes to the finance problem page setup

--- skillsprint/frontend/src/App.js
+++ skillsprint/frontend/src/App.js
@@ -6,6 +6,7 @@ import LanguageHome from './language/LanguageHome';
 import FinanceHome from './finance/FinanceHome';
 import CodingHome from './coding/coding-home';
 import React from 'react';
+import FinanceProblems from './finance/FinanceProblems';
 
 function App() {
   return (
@@ -32,6 +33,12 @@ function App() {
             <>
               <Navbar />
               <FinanceHome />
+            </>
+          }/>
+          <Route path="/finance-problems" element={
+            <>
+              <Navbar />
+              <FinanceProblems />
             </>
           }/>
         </Routes>
